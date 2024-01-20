e49b2fc18f8064a033f02237694a52f813c1e178
Crescens <crescencio.ps.code@gmail.com>
2024-01-20 17:43:03 -0600

    Genetic algorithm

--- GeneticAlgorithm/AlgorithmPrincipal.py
+++ GeneticAlgorithm/AlgorithmPrincipal.py
@@ -1,26 +1,27 @@
 import math
 import random
-from sympy import cos, sin, symbols
+from sympy import cos, sin, symbols, rad, sympify
 import sympy as sp
 from Layout.Models.Table import insertar_datos
+import random
 
 
 class Individuo:
     id_counter = 1
-    def __init__(self, individuo, i, minimo, maximo, delta_deseada, ecuacion):
+    def __init__(self, individuo, minimo, maximo, delta_deseada, ecuacion):
         self.id = Individuo.id_counter
         Individuo.id_counter += 1
         self.individuo = individuo  
-        self.i = i  
+        self.i = int(''.join(map(str, individuo)), 2)
         self.x = self.calcular_x(minimo, maximo, delta_deseada)
         self.fx = self.calcular_fx(ecuacion)
         
     def __str__(self):
-        return f"Individuo {self.id}: {self.individuo}, i: {self.i}, x: {self.x}, fx: {self.fx}"
+        return str(self.individuo)
 
     def calcular_bits (self, minimo, maximo, delta_deseada):
         rango = maximo-minimo
-        saltos = math.ceil(rango/delta_deseada)+1
+        saltos = (rango/delta_deseada)+1
         bits = math.ceil(math.log2(saltos))
         return bits
     
@@ -33,38 +34,42 @@ class Individuo:
             delta = delta_deseada
         elif delta < delta_deseada:
             delta = delta
-        print(delta)
-        
         x = minimo + self.i * delta
         
-        return float("{:.2f}".format(x))
+        return x
 
     def calcular_fx(self, ecuacion):
         try:
             var = sp.symbols('x')
-            resultado = sp.sympify(ecuacion).subs(var, math.radians(self.x))
-            return float("{:.2f}".format(resultado))
+            # Verificar si la ecuación contiene alguna función trigonométrica
+            if 'sin' in ecuacion or 'cos' in ecuacion or 'tan' in ecuacion:
+                # Convertir self.x a radianes
+                x_value = math.radians(self.x)
+            else:
+                x_value = self.x
+            # Convertir la ecuación a una expresión sympy y sustituir la variable por su valor
+            resultado = sympify(ecuacion).subs(var, x_value)
+            # Evaluar la expresión
+            resultado = resultado.evalf()
+            print("Resultado de la ecuación:", resultado)
+            return resultado
         except Exception as e:
             print("Error al calcular fx:", e)
             return None
-    
-
 
 class AlgoritmoGenetico:
-        def __init__(self, minimo, maximo, delta_deseada,ecuacion,initial_population, limit_population, probabilidad_cruce, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar):
-            self.ejecutar_algoritmo(minimo, maximo, delta_deseada, ecuacion, initial_population, limit_population, probabilidad_cruce, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar)
+        def __init__(self, minimo, maximo, delta_deseada,ecuacion,initial_population, limit_population, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar,tree):
+            self.poblacion_final = []
+            self.ejecutar_algoritmo(minimo, maximo, delta_deseada, ecuacion, initial_population, limit_population, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar,tree)
         
         def inicializar_populacion(self, minimo, maximo, delta_deseada, ecuacion, initial_population):
             poblacion = []
             for _ in range(initial_population):
                 # Generar un número binario aleatorio de longitud n
-                bits = Individuo.calcular_bits(self,minimo, maximo, delta_deseada)
-                individuo_binario = "".join(str(random.randint(0, 1)) for _ in range(bits))
-
-                # Convertir el número binario a decimal
-                i = int(individuo_binario, 2)
-                individuo = Individuo(individuo_binario, i, minimo, maximo, delta_deseada, ecuacion)
-                individuo.individuo = individuo_binario  # asignar el número binario al individuo
+                bits = Individuo.calcular_bits(self, minimo, maximo, delta_deseada)
+                individuo_binario = [random.randint(0, 1) for _ in range(bits)]
+                
+                individuo = Individuo(individuo_binario, minimo, maximo, delta_deseada, ecuacion)
 
                 poblacion.append(individuo)
                 print("Individuo creado:")
@@ -77,6 +82,13 @@ class AlgoritmoGenetico:
             return poblacion
         
         def seleccionar_mejores(self, poblacion, porcentaje_cruce, maximizar):
+            # print("Población que estoy recibiendo:")
+            # for individuo in poblacion:
+            #     print("Individuo (i):", individuo.i)
+            #     print("Individuo (fx):", individuo.fx)
+            #     print("--------------------")
+
+            poblacion = [x for x in poblacion if not isinstance(x, list)]
             if maximizar:
                 poblacion_ordenada = sorted(poblacion, key=lambda x: x.fx, reverse=True)
             else:
@@ -102,28 +114,100 @@ class AlgoritmoGenetico:
                 # Elimina el mejor individuo de la lista de mejores individuos
                 mejores_individuos.remove(mejor_individuo)
 
-            print("Parejas seleccionadas:")
-            for i, pareja in enumerate(parejas):
-                print(f"Pareja {i+1}: Individuo {pareja[0].i}, Individuo {pareja[1].i}")
+            # # Imprime las parejas en la consola
+            # for pareja in parejas:
+            #     print("Pareja:")
+            #     print("Individuo 1 (binario):", pareja[0].individuo)
+            #     print("Individuo 1 (i):", pareja[0].i)
+            #     print("Individuo 2 (binario):", pareja[1].individuo)
+            #     print("Individuo 2 (i):", pareja[1].i)
+            #     print("--------------------")
 
             return parejas
         
-       
-        def ejecutar_algoritmo(self, minimo, maximo, delta_deseada, ecuacion, initial_population, limit_population, probabilidad_cruce, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar):
+        def cruzar(self, parejas):
+            descendientes = []
+            for pareja in parejas:
+                individuo1, individuo2 = pareja
+                longitud_minima = min(len(individuo1.individuo), len(individuo2.individuo))
+                puntos_cruza = random.randint(1, longitud_minima)  # Random number of crossover points
+                posiciones_cruza = random.sample(range(1, longitud_minima), min(puntos_cruza, longitud_minima - 1))  # Random positions for crossover points
+
+                descendiente1 = []
+                descendiente2 = []
+                for i in range(len(individuo1.individuo)):
+                    if i in posiciones_cruza:
+                        descendiente1.append(individuo2.individuo[i])
+                        descendiente2.append(individuo1.individuo[i])
+                    else:
+                        descendiente1.append(individuo1.individuo[i])
+                        descendiente2.append(individuo2.individuo[i])
+
+                # # Convierte la versión binaria a una cadena de caracteres
+                # descendiente1_str = ''.join(map(str, descendiente1))
+                # descendiente2_str = ''.join(map(str, descendiente2))
+
+                # # Convierte la versión binaria a decimal
+                # descendiente1_decimal = int(descendiente1_str, 2)
+                # descendiente2_decimal = int(descendiente2_str, 2)
+
+                # # Imprime los descendientes en la consola
+                # print(f"Descendiente 1 (binario): {descendiente1_str}")
+                # print(f"Descendiente 1 (decimal): {descendiente1_decimal}")
+                # print(f"Descendiente 2 (binario): {descendiente2_str}")
+                # print(f"Descendiente 2 (decimal): {descendiente2_decimal}")
+                # print("--------------------")
+
+                descendientes.append(descendiente1)
+                descendientes.append(descendiente2)
+
+            return descendientes
+
+        def mutar_poblacion(self, poblacion, individual_mutation, gene_mutation):
+            for i in range(len(poblacion)):
+                if random.random() < individual_mutation:
+                    poblacion[i] = [1 if bit == 0 else 0 for bit in poblacion[i]]
+
+            for i in range(len(poblacion)):
+                for j in range(len(poblacion[i])):
+                    if random.random() < gene_mutation:
+                        poblacion[i][j] = 1 if poblacion[i][j] == 0 else 0
+            return poblacion
+        
+        def podar_poblacion(self, poblacion_final, limit_population, maximizar):
+            # Ordenar poblacion_final según la aptitud de los individuos
+            poblacion_final.sort(key=lambda individuo: individuo.fx, reverse=maximizar)
+            
+            # Mantener solo los primeros limit_population individuos
+            poblacion_final = poblacion_final[:limit_population]
+            
+            return poblacion_final
+        
+        def ejecutar_algoritmo(self, minimo, maximo, delta_deseada, ecuacion, initial_population, limit_population, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar,tree):
             poblacion = self.inicializar_populacion(minimo, maximo, delta_deseada, ecuacion, initial_population)
-            print("maximizar->",maximizar)
-            generaciones = [poblacion]
+            insertar_datos(tree, poblacion, es_poblacion_inicial=True)
+            
+            # Agregar la población inicial a poblacion_final
+            self.poblacion_final.extend(poblacion)
+        
             for _ in range(iteraciones):
-                print("Generación:", _+1)
-                parejas = self.seleccionar_mejores(poblacion, porcentaje_cruza_value, maximizar)
-                poblacion = [individuo for pareja in parejas for individuo in pareja]
-                # poblacion = self.cruzar_poblacion(poblacion, probabilidad_cruce)
-                # poblacion = self.mutar_poblacion(poblacion, individual_mutation, gene_mutation)
-                generaciones.append(poblacion) 
+                print("calculando generacion num ->", _+1)
+                parejas = self.seleccionar_mejores(self.poblacion_final, porcentaje_cruza_value, maximizar)  # Selecciona de la población final
+                cruza = self.cruzar(parejas)
+                poblacion_mutada = self.mutar_poblacion(cruza, individual_mutation, gene_mutation)
+                
+                # Crear nuevos objetos Individuo a partir de la población mutada y agregarlos a poblacion_final
+                for individuo_binario in poblacion_mutada:
+                    individuo = Individuo(individuo_binario, minimo, maximo, delta_deseada, ecuacion)
+                    self.poblacion_final.append(individuo)
+                
+                poblacion = poblacion_mutada  # Actualiza la población con los individuos mutados
+                self.poblacion_final = self.podar_poblacion(self.poblacion_final, limit_population, maximizar)
+            # Insertar los resultados de la poda en la tabla
+            insertar_datos(tree, self.poblacion_final, es_poblacion_inicial=False)
+
             print("Población final:")
-            for i, generacion in enumerate(generaciones):
-                print(f"Generación {i+1}:")
-                for individuo in generacion:
-                    print(individuo)
-            # insertar_datos(poblacion)
-            return generaciones
+            for individuo in self.poblacion_final:
+                print(individuo.id, individuo.individuo ,individuo.i, individuo.x, individuo.fx)
+            
+            return self.poblacion_final
--- Layout/Interface.py
+++ Layout/Interface.py
@@ -15,32 +15,31 @@ def Interface():
     ttk.Label(frame_init, text="f(x):").grid(
         column=0, row=1, sticky=tk.W, padx=5, pady=5)
     equation = ttk.Entry(frame_init, width=entry_width)
-    equation.insert(0, "sin(x)+cos(x)")
+    equation.insert(0, "sin(x) + cos(x)")
     equation.grid(column=1, row=1, sticky=(tk.W, tk.E), pady=1)
     
     ttk.Label(frame_init, text="Valor minimo para X:").grid(
         column=0, row=2, sticky=tk.W, pady=1)
     min_result_entry = ttk.Entry(frame_init, width=entry_width)
-    min_result_entry.insert(0, "5")
+    min_result_entry.insert(0, "3")
     min_result_entry.grid(column=1, row=2, sticky=(tk.W, tk.E), pady=1)
     
     ttk.Label(frame_init, text="Valor máximo para X:").grid(
         column=0, row=3, sticky=tk.W, pady=1)
     max_result_entry = ttk.Entry(frame_init, width=entry_width)
-    max_result_entry.insert(0, "25")
+    max_result_entry.insert(0, "7")
     max_result_entry.grid(column=1, row=3, sticky=(tk.W, tk.E), pady=1)
 
     ttk.Label(frame_init, text="Delta:").grid(
         column=0, row=5, sticky=tk.W, pady=1)
     delta_deseada = ttk.Entry(frame_init, width=entry_width)
-    delta_deseada.insert(0, "0.3")
+    delta_deseada.insert(0, "0.05")
     delta_deseada.grid(column=1, row=5, sticky=(tk.W, tk.E), pady=1)
     
     ttk.Label(frame_init, text="Generaciones:").grid(
         column=0, row=4, sticky=tk.W, pady=1)
     iteraciones = ttk.Entry(frame_init, width=entry_width)
-    iteraciones.insert(0, "5")
-
+    iteraciones.insert(0, "4")
     iteraciones.grid(column=1, row=4, sticky=(tk.W, tk.E), pady=1)
 
 
@@ -48,14 +47,14 @@ def Interface():
     ttk.Label(frame_poblacion, text="Tamaño Inicial:").grid(
         column=0, row=1, sticky=tk.W, pady=1)
     initial_population = ttk.Entry(frame_poblacion, width=entry_width)
-    initial_population.insert(0, "20")
+    initial_population.insert(0, "3")
     initial_population.grid(column=1, row=1, sticky=(tk.W, tk.E), pady=1)
     set_widget_width(initial_population, entry_width)
 
     ttk.Label(frame_poblacion, text="Tamaño Máximo:").grid(
         column=0, row=2, sticky=tk.W, pady=1)
     limit_population = ttk.Entry(frame_poblacion, width=entry_width)
-    limit_population.insert(0, "50")
+    limit_population.insert(0, "7")
     limit_population.grid(column=1, row=2, sticky=(tk.W, tk.E), pady=1)
     set_widget_width(limit_population, entry_width)
 
@@ -63,30 +62,25 @@ def Interface():
     ttk.Label(frame_mutacion, text="Por Individuo:").grid(
         column=0, row=1, sticky=tk.W, pady=1)
     individual_mutation = ttk.Entry(frame_mutacion, width=entry_width)
-    individual_mutation.insert(0, "0.25")
+    individual_mutation.insert(0, "0.9")
     individual_mutation.grid(column=1, row=1, sticky=(tk.W, tk.E), pady=1)
     set_widget_width(individual_mutation, entry_width)
 
     ttk.Label(frame_mutacion, text="Por Gen:").grid(
         column=0, row=2, sticky=tk.W, pady=1)
     gene_mutation = ttk.Entry(frame_mutacion, width=entry_width)
-    gene_mutation.insert(0, "0.15")
+    gene_mutation.insert(0, "0.9")
     gene_mutation.grid(column=1, row=2, sticky=(tk.W, tk.E), pady=1)
     set_widget_width(gene_mutation, entry_width)
 
     frame_cruza = section(root, "Probabilidad de Cruza")
-    ttk.Label(frame_cruza, text="Probabilidad de Cruza:").grid(
-        column=0, row=1, sticky=tk.W, pady=1)
-    entry_prob_cruza = ttk.Entry(frame_cruza, width=entry_width)
-    entry_prob_cruza.insert(0, "0.50")
-    entry_prob_cruza.grid(column=1, row=1, sticky=(tk.W, tk.E), pady=1)
-    set_widget_width(entry_prob_cruza, entry_width)
+    
     ttk.Label(frame_cruza, text="Porcentaje de cruce:").grid(
         column=0, row=2, sticky=tk.W, pady=1)
     porcentaje_cruza = ttk.Entry(frame_cruza, width=entry_width)
-    porcentaje_cruza.insert(0, "0.50")
+    porcentaje_cruza.insert(0, "0.9")
     porcentaje_cruza.grid(column=1, row=2, sticky=(tk.W, tk.E), pady=1)
-    set_widget_width(entry_prob_cruza, entry_width)
+    set_widget_width(porcentaje_cruza, entry_width)
 
     frame_table = ttk.Frame(root, padding=10)
     frame_table.grid(row=5, column=0, columnspan=2, pady=10)
@@ -95,6 +89,7 @@ def Interface():
     
     
     def minimizar():
+        AlgoritmoGenetico.poblacion_final = []
         tree.delete(*tree.get_children())
         maximizar = False
             
@@ -107,19 +102,19 @@ def Interface():
         iteration = int(iteraciones.get())
         
         initial_population_value = int(initial_population.get())
-        entry_prob_cruza_value = float(entry_prob_cruza.get())
         limit_population_value = int(limit_population.get())
         individual_mutation_value = float(individual_mutation.get())
         gene_mutation_value = float(gene_mutation.get())
         porcentaje_cruza_value = float(porcentaje_cruza.get())
         
-        AlgoritmoGenetico(minimo, maximo, delta_deseada_value, equation_value, initial_population_value, limit_population_value, entry_prob_cruza_value, individual_mutation_value, gene_mutation_value, iteration, porcentaje_cruza_value, maximizar)
+        AlgoritmoGenetico(minimo, maximo, delta_deseada_value, equation_value, initial_population_value, limit_population_value, individual_mutation_value, gene_mutation_value, iteration, porcentaje_cruza_value, maximizar,tree)
 
     button = ttk.Button(frame_init, text="Minimizacion",
                         command=minimizar)
     button.grid(column=2, row=1, sticky=tk.W, padx=5, pady=5)
 
     def maximizar():
+        AlgoritmoGenetico.poblacion_final = []
         tree.delete(*tree.get_children())
         maximizar = True
             
@@ -132,13 +127,12 @@ def Interface():
         iteration = int(iteraciones.get())
         
         initial_population_value = int(initial_population.get())
-        entry_prob_cruza_value = float(entry_prob_cruza.get())
         limit_population_value = int(limit_population.get())
         individual_mutation_value = float(individual_mutation.get())
         gene_mutation_value = float(gene_mutation.get())
         porcentaje_cruza_value = float(porcentaje_cruza.get())
         
-        AlgoritmoGenetico(minimo, maximo, delta_deseada_value, equation_value, initial_population_value, limit_population_value, entry_prob_cruza_value, individual_mutation_value, gene_mutation_value, iteration, porcentaje_cruza_value, maximizar)
+        AlgoritmoGenetico(minimo, maximo, delta_deseada_value, equation_value, initial_population_value, limit_population_value, individual_mutation_value, gene_mutation_value, iteration, porcentaje_cruza_value, maximizar)
         
 
 
