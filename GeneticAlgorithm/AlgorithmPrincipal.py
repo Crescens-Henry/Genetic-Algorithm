51c6de2a43a5f10594bf65ed5e2161cc90e4209a
Crescens <crescencio.ps.code@gmail.com>
2024-01-22 08:57:02 -0600

    qick fix

--- GeneticAlgorithm/AlgorithmPrincipal.py
+++ GeneticAlgorithm/AlgorithmPrincipal.py
@@ -67,32 +67,19 @@ class AlgoritmoGenetico:
             self.poblacion_final = []
             self.ejecutar_algoritmo(minimo, maximo, delta_deseada, ecuacion, initial_population, limit_population, individual_mutation, gene_mutation, iteraciones, porcentaje_cruza_value, maximizar,tree)
         
-        def inicializar_populacion(self, minimo, maximo, delta_deseada, ecuacion, initial_population):
+        def inicializar_poblacion(self, minimo, maximo, delta_deseada, ecuacion, initial_population):
             poblacion = []
             for _ in range(initial_population):
-                # Generar un número binario aleatorio de longitud n
                 bits = Individuo.calcular_bits(self, minimo, maximo, delta_deseada)
                 individuo_binario = [random.randint(0, 1) for _ in range(bits)]
                 
                 individuo = Individuo(individuo_binario, minimo, maximo, delta_deseada, ecuacion)
 
                 poblacion.append(individuo)
-                print("Individuo creado:")
-                print("ID:", individuo.id)
-                print("Individuo:", individuo.individuo)
-                print("i:", individuo.i)
-                print("x:", individuo.x)
-                print("fx:", individuo.fx)
-                print("--------------------")
+                
             return poblacion
         
         def seleccionar_mejores(self, poblacion, porcentaje_cruce, maximizar):
-            # print("Población que estoy recibiendo:")
-            # for individuo in poblacion:
-            #     print("Individuo (i):", individuo.i)
-            #     print("Individuo (fx):", individuo.fx)
-            #     print("--------------------")
-
             poblacion = [x for x in poblacion if not isinstance(x, list)]
             if maximizar:
                 poblacion_ordenada = sorted(poblacion, key=lambda x: x.fx, reverse=True)
@@ -104,29 +91,14 @@ class AlgoritmoGenetico:
             parejas = []
             parejas_formadas = set()
 
-            # Mientras haya suficientes individuos para formar una pareja
             while len(mejores_individuos) >= 2:
-                # El mejor individuo es siempre el primero de la lista
                 mejor_individuo = mejores_individuos[0]
-                # Para cada uno de los otros individuos
                 for otro_individuo in mejores_individuos[1:]:
-                    # Forma una pareja con el mejor individuo
                     pareja = (mejor_individuo.i, otro_individuo.i)
-                    # Verifica que la pareja no se haya formado antes
                     if pareja not in parejas_formadas:
                         parejas_formadas.add(pareja)
                         parejas.append((mejor_individuo, otro_individuo))
-                # Elimina el mejor individuo de la lista de mejores individuos
                 mejores_individuos.remove(mejor_individuo)
-
-            # # Imprime las parejas en la consola
-            # for pareja in parejas:
-            #     print("Pareja:")
-            #     print("Individuo 1 (binario):", pareja[0].individuo)
-            #     print("Individuo 1 (i):", pareja[0].i)
-            #     print("Individuo 2 (binario):", pareja[1].individuo)
-            #     print("Individuo 2 (i):", pareja[1].i)
-            #     print("--------------------")
 
             return parejas
         
@@ -135,9 +107,9 @@ class AlgoritmoGenetico:
             for pareja in parejas:
                 individuo1, individuo2 = pareja
                 longitud_minima = min(len(individuo1.individuo), len(individuo2.individuo))
-                puntos_cruza = random.randint(1, longitud_minima)  # Random number of crossover points
-                posiciones_cruza = random.sample(range(1, longitud_minima), min(puntos_cruza, longitud_minima - 1))  # Random positions for crossover points
-
+                puntos_cruza = random.randint(1, longitud_minima)
+                posiciones_cruza = random.sample(range(1, longitud_minima), min(puntos_cruza, longitud_minima - 1))  
+                
                 descendiente1 = []
                 descendiente2 = []
                 for i in range(len(individuo1.individuo)):
@@ -147,21 +119,6 @@ class AlgoritmoGenetico:
                     else:
                         descendiente1.append(individuo1.individuo[i])
                         descendiente2.append(individuo2.individuo[i])
-
-                # # Convierte la versión binaria a una cadena de caracteres
-                # descendiente1_str = ''.join(map(str, descendiente1))
-                # descendiente2_str = ''.join(map(str, descendiente2))
-
-                # # Convierte la versión binaria a decimal
-                # descendiente1_decimal = int(descendiente1_str, 2)
-                # descendiente2_decimal = int(descendiente2_str, 2)
-
-                # # Imprime los descendientes en la consola
-                # print(f"Descendiente 1 (binario): {descendiente1_str}")
-                # print(f"Descendiente 1 (decimal): {descendiente1_decimal}")
-                # print(f"Descendiente 2 (binario): {descendiente2_str}")
-                # print(f"Descendiente 2 (decimal): {descendiente2_decimal}")
-                # print("--------------------")
 
                 descendientes.append(descendiente1)
                 descendientes.append(descendiente2)
@@ -180,10 +137,8 @@ class AlgoritmoGenetico:
             return poblacion
         
         def podar_poblacion(self, poblacion_final, limit_population, maximizar):
-            # Ordenar poblacion_final según la aptitud de los individuos
             poblacion_final.sort(key=lambda individuo: individuo.fx, reverse=maximizar)
             
-            # Mantener solo los primeros limit_population individuos
             poblacion_final = poblacion_final[:limit_population]
             
             return poblacion_final
@@ -199,7 +154,7 @@ class AlgoritmoGenetico:
             mejores_resultados = []
             promedio_resultados = []
             peores_resultados = []
-            poblacion = self.inicializar_populacion(minimo, maximo, delta_deseada, ecuacion, initial_population)
+            poblacion = self.inicializar_poblacion(minimo, maximo, delta_deseada, ecuacion, initial_population)
             insertar_datos(tree, poblacion, es_poblacion_inicial=True)
             
             # Agregar la población inicial a poblacion_final
@@ -224,15 +179,17 @@ class AlgoritmoGenetico:
                         peor_resultado = max(individuo.fx for individuo in self.poblacion_final) 
                     promedio_resultado = sum(individuo.fx for individuo in self.poblacion_final) / len(self.poblacion_final)
                 
-                    x_values_x = [float(individuo.x) for individuo in self.poblacion_final]
-                    fx_values_y = [float(individuo.fx) for individuo in self.poblacion_final]
-                    print("x_values_x", x_values_x)
-                    print("fx_values_y", fx_values_y)
-                    listaAnimaciones.append(animarPlot(x_values_x, fx_values_y))
-                    if len(listaAnimaciones) == 0:
-                        print("No se generaron frames para la animación.")
+                x_values_x = [float(individuo.x) for individuo in self.poblacion_final]
+                fx_values_y = [float(individuo.fx) for individuo in self.poblacion_final]
+                    
+                animacion = animarPlot(x_values_x, fx_values_y)
                 nombre_video = "video" + str(_+1) + ".mp4"
                 listaVideos.append(nombre_video)
+                grabarVideo(animacion, nombre_video)
+                
+                listaAnimaciones.append(animacion)
+                if len(listaAnimaciones) == 0:
+                    print("No se generaron frames para la animación.")
                 
                 poblacion = poblacion_mutada  # Actualiza la población con los individuos mutados
                 self.poblacion_final = self.podar_poblacion(self.poblacion_final, limit_population, maximizar)
@@ -242,16 +199,17 @@ class AlgoritmoGenetico:
                 promedio_resultados.append(promedio_resultado)
                 peores_resultados.append(peor_resultado)
             
-            unirVariosVideos(listaAnimaciones, listaVideos)
-            fin = time.time()
-            tiempo_ejecucion = fin - inicio
-            print("Tiempo de ejecución:", tiempo_ejecucion, "segundos")
-            
-            # Insertar los resultados de la poda en la tabla
             insertar_datos(tree, self.poblacion_final, es_poblacion_inicial=False)
             x_values = list(range(iteraciones))
             LineChart.update_graph(x_values, mejores_resultados, promedio_resultados, peores_resultados, ax)
             plt.show()  # Asegúrate de llamar a plt.show() para mostrar la gráfica
+            
+            unirVariosVideos(listaAnimaciones, listaVideos)
+            fin = time.time()
+            tiempo_ejecucion = fin - inicio
+            print("Tiempo de ejecución:", tiempo_ejecucion, "segundos")
+            reproducirVideo("video_final.mp4")
+            # Insertar los resultados de la poda en la tabla
             
             print("Población final:")
             for individuo in self.poblacion_final:
--- Layout/Models/grabarPlots.py
+++ Layout/Models/grabarPlots.py
@@ -15,10 +15,12 @@ def animarPlot(x,y):
                 ymin = np.min([val for val in y[:i] if np.isfinite(val)])  # Solo considerar valores finitos
                 ymax = np.max([val for val in y[:i] if np.isfinite(val)])  # Solo considerar valores finitos
                 ax.set_xlim([1.1*np.min(x),1.1*np.max(x)])
-                ax.set_ylim([1.1*ymin,1.1*ymax])
+                epsilon = 0.01  # o cualquier valor pequeño
+                ax.set_ylim([1.1*ymin - epsilon, 1.1*ymax + epsilon])
             except TypeError:
                 print("La lista y contiene valores no numéricos.")
     animar = FuncAnimation(fig, actualizarPlot,range(len(x)),interval=0, cache_frame_data=False, repeat = False)
+    print ("intenta animar el plot")
     return fig, animar
 
 def grabarVideo(animacion,nombre_video):
@@ -33,21 +35,22 @@ def unirVariosVideos(listaAnimaciones,listaVideos):
         animar.save(listaVideos[i],writer = 'ffmpeg', fps = 60, dpi=100)
         plt.close(fig)
         videos.append(cv.VideoCapture(listaVideos[i]))
-        os.remove(listaVideos[i])
+
     ancho = int(videos[0].get(cv.CAP_PROP_FRAME_WIDTH))
     alto = int(videos[0].get(cv.CAP_PROP_FRAME_HEIGHT))
     fps = int(videos[0].get(cv.CAP_PROP_FPS))
     fourcc = cv.VideoWriter_fourcc(*'mp4v')
     video_combinado = cv.VideoWriter('video_final.mp4', fourcc, fps,(ancho,alto))    
+
     for video in videos:
         while True:
             ret, frame = video.read()
             if not ret:
                 break
-            video_combinado.write(frame)
-    for video in videos:
-        video.release()
-    video_combinado.release()
+            video_combinado.write(frame)  # Escribe el frame en el video combinado
+        video.release()  # Asegúrate de liberar el video antes de eliminarlo
+
+    video_combinado.release()  # No olvides liberar el video combinado al final
 
 def reproducirVideo(nombre_video):
     video = cv.VideoCapture(nombre_video)
